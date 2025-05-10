3ae09e317c6d4df2cd97ab1cda91adde11982cec
David Agredano <davidagredano@gmail.com>
2025-05-10 19:27:52 +0200

    fix: Improve error handling in mutex initialization

--- philo/mutexes.c
+++ philo/mutexes.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:12:21 by dagredan          #+#    #+#             */
-/*   Updated: 2025/04/29 19:28:13 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/05/10 19:27:11 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	mutexes_init(t_data *data)
 	int	i;
 
 	if (pthread_mutex_init(&data->mutexes.global, NULL) != 0)
-		return (-1);
+		return (error(data, "pthread_mutex_init", "mutexes_init", 0));
 	data->mutexes.global_initialized = 1;
 	i = 0;
 	while (i < data->philos.len)
 	{
 		if (pthread_mutex_init(&data->mutexes.forks[i], NULL) != 0)
-			return (-1);
+			return (error(data, "pthread_mutex_init", "mutexes_init", 0));
 		data->mutexes.forks_initialized++;
 		i++;
 	}
@@ -31,7 +31,7 @@ int	mutexes_init(t_data *data)
 	while (i < data->philos.len)
 	{
 		if (pthread_mutex_init(&data->mutexes.philos[i], NULL) != 0)
-			return (-1);
+			return (error(data, "pthread_mutex_init", "mutexes_init", 0));
 		data->mutexes.philos_initialized++;
 		i++;
 	}
